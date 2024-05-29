009f7f0bee49e48df93cb5f55d41d03dcfdb48a7
vipinsharma0999 <vipin.vi0333@gmail.com>
2024-05-29 14:31:42 +0530

    changes

--- src/app/(donation)/explore-more/page.js
+++ src/app/(donation)/explore-more/page.js
@@ -1,12 +1,12 @@
 'use client'
-import ExploreMainPoster from '@/components/Donation/ExploreMore/ExploreMainPoster'
+import ExploreMainPoster from '@/components/Donation/ExploreMore/01ExploreMainPoster/ExploreMainPoster'
 import MainPoster from '@/components/Donation/ExploreMore/MainPoster'
 import React from 'react'
 
 const ExploreMore = () => {
   return (
     <>
-      <ExploreMainPoster />
+       <ExploreMainPoster />
        {/* <MainPoster /> */}
     </>
   )
